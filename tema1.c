3455a7614fc0368b57cc86bc30d310b253843681
doriham <doriham123@gmail.com>
2022-03-21 15:57:53 -0300

    add global variable, change function prototypes

--- tema1.c
+++ tema1.c
@@ -7,12 +7,19 @@
 #include<math.h>
 #define DELIMETER ","
 
+
+int * nums_global;
+
 /*prototypes*/
 int * get_int_argv(char c_nums[] , char * delimeter);
 int start_process(int prN , int * numbers);
-void print_header(int arr[] , int prN , int n);
-void print_arrLevel(int arr[], int level, int counter , int prN , int n);
-void print_treeMap(int arr[], int prN , int n);
+void print_header(int prN , int n);
+void print_arrLevel(int level, int counter , int prN , int n);
+void print_treeMap( int prN , int n);
+int merge_sort_process(int left , int right , int prN , int maxProcess);
+void merge_process(int left , int middle , int right , int prN);
+
+void print_array(int left , int right);
 /*end of prototypes*/
 
 int main(int argc , char * argv[]){
@@ -22,7 +29,6 @@ int main(int argc , char * argv[]){
     }
     int nbr_processes = atoi(argv[1]);
     int * numbers = get_int_argv(argv[2] , DELIMETER);
-    printf("%d\n" , numbers[0]);
     if(numbers != NULL && numbers[0] > 1){
         start_process(nbr_processes , numbers);
     }else{
@@ -33,6 +39,7 @@ int main(int argc , char * argv[]){
         printf("---------------------\n");
     }
     free(numbers);
+    free(nums_global);
     return 0;
 }
 
@@ -61,23 +68,43 @@ int * get_int_argv(char c_nums[] , char * delimeter){
 
 int start_process(int prN , int * numbers){
     int i,j;
-    int nums[numbers[0]]; ///nums to sort
+    nums_global = (int *) malloc(sizeof(int)*numbers[i]); 
     for(i = 0 ; i < numbers[0] ; i++)
-        nums[i] = numbers[i + 1];
-    pid_t childPID;
+        nums_global[i] = numbers[i + 1];
     int left,right;
+
+    printf("===Esquema de arbol===\n");
     //print nums mapping 
-    print_treeMap(nums, prN, numbers[0]);
+    print_treeMap(prN, numbers[0]);
+    printf("===mapeos===\n");
+    merge_sort_process(0 , numbers[0] - 1 , 0 , prN );
     return 0;
 }
 
 
-void print_treeMap(int arr[], int prN , int n){
-    print_header(arr , prN , n);
+int merge_sort_process( int left , int right , int prN , int maxProcess){
+
+    return 0;
+}
+
+void merge_process(int left , int middle , int right , int prN){
+    printf("===procesamiento===\n");
 }
 
 
-void print_header(int arr[] , int prN , int n){
+void print_array( int left , int right){
+    int i;
+    for(int i = left; i <= right ; i++)
+        printf("%d," , nums_global[i]);
+    printf("\n");
+}
+
+void print_treeMap(int prN , int n){
+    print_header(prN , n);
+}
+
+
+void print_header(int prN , int n){
     int pr_count = 0;
     int i,j;
     int level = 0;
@@ -85,20 +112,20 @@ void print_header(int arr[] , int prN , int n){
     while(pr_count < prN){
         counter = 0;
         for(i = 0 ; i < pow(2 , level) ; i++ ){
-            for(j = n/4  ; j >= level ; j--)
+            for(j = n/4  ; j >= level ; j--) //print tabs
                 printf("\t");
             printf("Process %d" , pr_count++);
             if(pr_count >= prN) break;
             counter++;
         }
         printf("\n");
-        print_arrLevel(arr,level, counter , pr_count , n);
+        print_arrLevel(level, counter , pr_count , n);
         printf("\n");
         level++;
     }
 }
 
-void print_arrLevel(int arr[], int level, int counter , int prN , int n){
+void print_arrLevel( int level, int counter , int prN , int n){
     int right = n/pow(2 , level) - 1;
     int left = 0;
     int i;
@@ -106,19 +133,15 @@ void print_arrLevel(int arr[], int level, int counter , int prN , int n){
 
     while(right < n && count <= counter){
         //print tabs
-        for(i = n/4 ; i >= level ; i--)
+        for(i = n/4; i >= level ; i--)
             printf("\t");
         //print arr
         for(i = left ; i <= right ; i++)
-            printf("%d," , arr[i]);
+            printf("%d," , nums_global[i]);
         left = i;
         right += n/pow(2 , level);
         if(right >= n && left < n)
             right = n - 1;
         count++;
     }
-}
-
-void merge(int arr[] , int left , int middle, int right , int prNumber){
-
 }
